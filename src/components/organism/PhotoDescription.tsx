259762552fa2d5e3bc686683a78be08412437f2c
carlos123asd <carlos-medin@hotmail.com>
2025-04-29 17:07:44 +0200

    ADD: style align image atropos

--- src/components/organism/PhotoDescription.tsx
+++ src/components/organism/PhotoDescription.tsx
@@ -3,7 +3,7 @@ import TitSection from "../atoms/TitSection";
 
 export default function PhotoDescription({url, descripcion, alt}:{url:string,descripcion:string|null,alt:string|null}){
     return <>
-        <Atropos>
+        <Atropos style={{alignSelf:"flex-start"}}>
             <img width={800} height={600} src={url} alt={descripcion ? descripcion : "image not found"} />
         </Atropos>
         <TitSection titulo={alt === descripcion ? "Photo Description" : alt} subtitulo={descripcion} />
