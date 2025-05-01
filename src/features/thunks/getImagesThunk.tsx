fa0e9fb6a5cfd33708963758d8e3642c67c93e1f
carlos123asd <carlos-medin@hotmail.com>
2025-05-02 01:59:05 +0200

    Update: url thunk get more images

--- src/features/thunks/getImagesThunk.tsx
+++ src/features/thunks/getImagesThunk.tsx
@@ -2,7 +2,7 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 export const getImagesThunk = createAsyncThunk('imagesList', async () => {
     try {
-        const response = await fetch(`https://api.unsplash.com/photos?page=1&client_id=${import.meta.env.VITE_ACCESS_KEY}`);
+        const response = await fetch(`https://api.unsplash.com/photos?page=1&per_page=20&client_id=${import.meta.env.VITE_ACCESS_KEY}`);
         if(response.ok){
             const json = await response.json();
             return json
